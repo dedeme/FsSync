19dbe4da15af3c42d66c69ffb6cb41d3eab92877
dedeme <dedeme@terra.com>
2014-11-10 14:39:37 +0100

    First commit

---
+++ src/ftpsync/FtpSync.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 10-nov-2014 ºDeme
+ *
+ * This file is part of 'ftpsync'.
+ *
+ * 'ftpsync' is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * 'ftpsync' is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with 'ftpsync'.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package ftpsync;
+
+
+/**
+ *
+ * @version 1.0
+ * @since 10-Nov-2014
+ * @author deme
+ */
+public class FtpSync {
+
+  /**
+    * @param args the command line arguments
+    */
+  public static void main(String[] args) {
+    System.out.println("here");
+  }
+
+}
---
+++ test/ftpsync/SynchronizerTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 10-nov-2014 ºDeme
+ *
+ * This file is part of 'ftpsync'.
+ *
+ * 'ftpsync' is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * 'ftpsync' is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with 'ftpsync'.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ftpsync;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author deme
+ */
+public class SynchronizerTest {
+
+  @Test
+  public void SynchronizerTest() {
+    assertTrue (true);
+  }
+
+  @Test
+  public void SynchronizerTest2() {
+    assertEquals (3, 2+1);
+  }
+
+}
