6164379e3c3ced5f894292f0fb803aaeb2e2436f
dedeme <dedeme@terra.com>
2014-11-12 01:42:35 +0100

    Add Algor

---
+++ src/ftpsync/Algor.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 11-nov-2014 ºDeme
+ *
+ * This file is part of 'ftpsync'.
+ *
+ * 'ftpsync' is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * 'ftpsync' is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with 'ftpsync'.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ftpsync;
+
+import java.util.List;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+/**
+ *
+ * @version 1.0
+ * @since 11-Nov-2014
+ * @author deme
+ */
+public class Algor {
+  static <S, T> String operate (
+    List<S> source,
+    List<T> target,
+    BiFunction<S, Iterable<T>, String> mustOperate,
+    Function<S, String> operation
+  ){
+    String r = "";
+    for (S s : source) {
+      String must = mustOperate.apply(s, target);
+      if (must == null)
+        r = operation.apply(s);
+      else
+        r = must;
+      if (!r.equals(""))
+        break;
+    }
+    return r;
+  }
+
+  /**
+   * Synchronizes elements of source with elements of target.
+   * @param <S> Type of source element (original)
+   * @param <T> Type of target element (copy)
+   * @param source List of elements S
+   * @param target List of elements T
+   * @param mustCopy Indicates if a copy has to be made. Returns:
+   * <dl>
+   * <dt>null</dt><dd>Copy has to be made.</dd>
+   * <dt>""</dt><dd>Copy has not to be made.</dd>
+   * <dt>message</dt><dd>An error has happened.</dd>
+   * </dl>
+   * @param copy Operation to actualize an element which is obsolete.
+   * @param mustDelete Indicates if a deletion has to be made. Returns:
+   * <dl>
+   * <dt>null</dt><dd>Deletion has to be made.</dd>
+   * <dt>""</dt><dd>Deletion has not to be made.</dd>
+   * <dt>message</dt><dd>An error has happened.</dd>
+   * </dl>
+   * @param delete Operation to delete an element in copy which is not in
+   *   origen.
+   * @return An empty string if synchronization was successful or a message
+   *   if an error happened.
+   */
+  public static <S, T> String sync(
+    List<S> source,
+    List<T> target,
+    BiFunction<S, Iterable<T>, String> mustCopy,
+    Function<S, String> copy,
+    BiFunction<T, Iterable<S>, String> mustDelete,
+    Function<T, String> delete
+  ) {
+    String r = operate(source, target, mustCopy, copy);
+    if (r.equals("")) {
+      r = operate(target, source, mustDelete, delete);
+    }
+    return r;
+  }
+}
--- test/ftpsync/SynchronizerTest.java
+++ test/ftpsync/SynchronizerTest.java
@@ -17,6 +17,9 @@
  */
 package ftpsync;
 
+import java.util.ArrayList;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -26,14 +29,168 @@ import static org.junit.Assert.*;
  */
 public class SynchronizerTest {
 
+  Function<CharPos, String> makeCopy(ArrayList<CharPos> t) {
+    return (CharPos s) -> {
+      int pos = s.getPos();
+      for (int i = t.size(); i <= pos; ++i) {
+        t.add(new CharPos(' ', i));
+      }
+      t.set(s.getPos(), s);
+      return "";
+    };
+  }
+
+  Function<CharPos, String> makeDelete(ArrayList<CharPos> ts) {
+    return (CharPos t) -> {
+      ts.set(t.getPos(), null);
+      return "";
+    };
+  }
+
   @Test
   public void SynchronizerTest() {
-    assertTrue (true);
+    BiFunction<CharPos, Iterable<CharPos>, String> mustCopy
+      = (CharPos ch, Iterable<CharPos> it) -> {
+        int count = 0;
+        char chc = ch.getCh();
+        int chp = ch.getPos();
+        if (Character.isDigit(chc)) {
+          return String.format("Character in position %d is a digit (%c)",
+            chp, chc);
+        }
+        for (CharPos c : it) {
+          if (chp == count) {
+            char cc = c.getCh();
+            if (chc != cc) {
+              return null;
+            }
+          }
+          ++count;
+        }
+        if (chp >= count) {
+          return null;
+        }
+        return "";
+      };
+
+    BiFunction<CharPos, Iterable<CharPos>, String> mustDelete
+      = (CharPos ch, Iterable<CharPos> it) -> {
+        int count = 0;
+        for (CharPos c : it) {
+          ++count;
+        }
+        if (ch.getPos() >= count) {
+          return null;
+        }
+        return "";
+      };
+
+    Function<String, ArrayList<CharPos>> toList
+      = (String s) -> {
+        ArrayList<CharPos> r = new ArrayList<>();
+        for (int i = 0; i < s.length(); ++i) {
+          r.add(new CharPos(s.charAt(i), i));
+        }
+        return r;
+      };
+
+    Function<ArrayList<CharPos>, String> toString
+      = (ArrayList<CharPos> a) -> {
+        StringBuilder s = new StringBuilder();
+        a.stream().forEach((chp) -> {
+          if (chp != null) {
+            s.append(chp.getCh());
+          }
+        });
+        return s.toString();
+      };
+
+    ArrayList<CharPos> s0 = toList.apply("");
+    ArrayList<CharPos> t0 = toList.apply("");
+    assertEquals("",
+      Algor.sync(
+        s0, t0,
+        mustCopy, makeCopy(t0),
+        mustDelete, makeDelete(t0)
+      ));
+    assertTrue(toString.apply(s0).equals(toString.apply(t0)));
+
+    s0 = toList.apply("AbC");
+    t0 = toList.apply("");
+    assertEquals("",
+      Algor.sync(
+        s0, t0,
+        mustCopy, makeCopy(t0),
+        mustDelete, makeDelete(t0)
+      ));
+    assertTrue(toString.apply(s0).equals(toString.apply(t0)));
+
+    s0 = toList.apply("");
+    t0 = toList.apply("AbC");
+    assertEquals("",
+      Algor.sync(
+        s0, t0,
+        mustCopy, makeCopy(t0),
+        mustDelete, makeDelete(t0)
+      ));
+    assertTrue(toString.apply(s0).equals(toString.apply(t0)));
+
+    s0 = toList.apply("ABC");
+    t0 = toList.apply("AbC");
+    assertEquals("",
+      Algor.sync(
+        s0, t0,
+        mustCopy, makeCopy(t0),
+        mustDelete, makeDelete(t0)
+      ));
+    assertTrue(toString.apply(s0).equals(toString.apply(t0)));
+    assertTrue("ABC".equals(toString.apply(t0)));
+
+    s0 = toList.apply("ABC");
+    t0 = toList.apply("sAbCmjr");
+    assertEquals("",
+      Algor.sync(
+        s0, t0,
+        mustCopy, makeCopy(t0),
+        mustDelete, makeDelete(t0)
+      ));
+    assertTrue(toString.apply(s0).equals(toString.apply(t0)));
+    assertTrue("ABC".equals(toString.apply(t0)));
+
+    s0 = toList.apply("A0C");
+    t0 = toList.apply("sAbCmjr");
+    assertEquals("Character in position 1 is a digit (0)",
+      Algor.sync(
+        s0, t0,
+        mustCopy, makeCopy(t0),
+        mustDelete, makeDelete(t0)
+      ));
+    assertTrue("A0C".equals(toString.apply(s0)));
+    assertTrue("AAbCmjr".equals(toString.apply(t0)));
   }
 
   @Test
   public void SynchronizerTest2() {
-    assertEquals (3, 2+1);
+    assertEquals(3, 2 + 1);
   }
 
+}
+
+class CharPos {
+
+  char ch;
+  int pos;
+
+  public CharPos(char ch, int pos) {
+    this.ch = ch;
+    this.pos = pos;
+  }
+
+  public char getCh() {
+    return ch;
+  }
+
+  public int getPos() {
+    return pos;
+  }
 }
